ec7eff44b9146f723da50db02d22cafcc9f56f67
unknown <eliezra236@gmail.com>
2021-11-10 00:51:45 +0200

    deleted some temp files

--- test.js
+++
@@ -1,9 +0,0 @@
-const capture_website = import('capture-website');
-
-async function testCapture() {
-    const capture = await capture_website;
-    const result = await capture.default.file("https://github.com/eliezra236/app-to-monitor/pull/1", 'screenshot.png');
-    console.log(result);
-}
-
-testCapture();
